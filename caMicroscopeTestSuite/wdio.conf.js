cdedecaedb061628c60f465ffdb46d0df76fe1c1
YHE54 <heyuan0755@gmail.com>
2017-01-29 15:39:30 -0500

    Update wdio.conf.js

--- caMicroscopeTestSuite/wdio.conf.js
+++ caMicroscopeTestSuite/wdio.conf.js
@@ -32,7 +32,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    //maxInstances: 10,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -42,9 +42,10 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instance available you can make sure that not more than
         // 5 instance gets started at a time.
-        //maxInstances: 5,
-        //
-        browserName: 'chrome'
+        maxInstances: 5,
+        //browserName=chrome,maxInstances=5,platform=LINUX
+        browserName: 'chrome' 
+        platform:'LINUX'
     }],
     //
     // ===================
